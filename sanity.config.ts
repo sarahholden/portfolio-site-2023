822db97f78bf677ee0e7a4877cde63ed3bdcc7c3
Sarah Holden <sarahbethholden@gmail.com>
2023-10-14 16:19:37 -0700

    Setting up singletons in sanity config and schema

--- sanity.config.ts
+++ sanity.config.ts
@@ -30,6 +30,12 @@ const iframeOptions = {
   reload: { button: true },
 } satisfies IframeOptions
 
+// Define the actions that should be available for singleton documents
+const singletonActions = new Set(['publish', 'discardChanges', 'restore'])
+
+// Define the singleton document types
+const singletonTypes = new Set(['settings'])
+
 export default defineConfig({
   basePath: '/studio',
   name: 'project-name',
@@ -39,21 +45,39 @@ export default defineConfig({
   //edit schemas in './src/schemas'
   schema,
   plugins: [
+    // deskTool({
+    //   // `defaultDocumentNode` is responsible for adding a “Preview” tab to the document pane
+    //   // You can add any React component to `S.view.component` and it will be rendered in the pane
+    //   // and have access to content in the form in real-time.
+    //   // It's part of the Studio's “Structure Builder API” and is documented here:
+    //   // https://www.sanity.io/docs/structure-builder-reference
+    //   defaultDocumentNode: (S, { schemaType }) => {
+    //     return S.document().views([
+    //       // Default form view
+    //       S.view.form(),
+    //       // Preview
+    //       S.view.component(Iframe).options(iframeOptions).title('Preview'),
+    //     ])
+    //   },
+    // }),
     deskTool({
-      // `defaultDocumentNode` is responsible for adding a “Preview” tab to the document pane
-      // You can add any React component to `S.view.component` and it will be rendered in the pane
-      // and have access to content in the form in real-time.
-      // It's part of the Studio's “Structure Builder API” and is documented here:
-      // https://www.sanity.io/docs/structure-builder-reference
-      defaultDocumentNode: (S, { schemaType }) => {
-        return S.document().views([
-          // Default form view
-          S.view.form(),
-          // Preview
-          S.view.component(Iframe).options(iframeOptions).title('Preview'),
-        ])
-      },
+      structure: (S) =>
+        S.list()
+          .title('Content')
+          .items([
+            // Our singleton type has a list item with a custom child
+            S.listItem().title('Settings').id('settings').child(
+              // Instead of rendering a list of documents, we render a single
+              // document, specifying the `documentId` manually to ensure
+              // that we're editing the single instance of the document
+              S.document().schemaType('settings').documentId('settings'),
+            ),
+
+            // Regular document types
+            S.documentTypeListItem('post').title('Blog Posts'),
+          ]),
     }),
+
     // Add the "Open preview" action
     previewUrl({
       base: '/api/draft',
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -3,8 +3,10 @@ import { SchemaTypeDefinition } from 'sanity'
 import blockContent from './blockContent'
 import post from './post'
 import project from './project'
+import home from './singletons/home'
+import settings from './singletons/settings'
 
-export const schemaTypes = [post, project, blockContent]
+export const schemaTypes = [post, project, home, settings, blockContent]
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, blockContent, project],
+  types: [post, project, home, settings, blockContent],
 }
