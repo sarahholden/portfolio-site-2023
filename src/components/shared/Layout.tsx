c2d196f34a80442cf7046c6b9a731497f6f59588
Sarah Holden <sarahbethholden@gmail.com>
2023-10-18 11:38:08 -0700

    JSX for Layout component, not yet styled

--- src/components/shared/Layout.tsx
+++ src/components/shared/Layout.tsx
@@ -1,4 +1,5 @@
 import { PortableText } from '@portabletext/react'
+import Link from 'next/link'
 
 import { SettingsPayload } from '~/types'
 
@@ -19,38 +20,62 @@ export default function Layout({
 }: LayoutProps) {
   return (
     <div>
-      <header>Header Here!</header>
-      <br />
-      <br />
-      <br />
-      <main>{children}</main>
-      <br />
-      <br />
-      <br />
-      <footer>
-        <div className="post__content">
-          <PortableText value={settings?.footer} />
+      <header className="header">
+        <div className="header__left">
+          <Link href="/" className="header__icon" title="Take me home">
+            <svg
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 60 60"
+            >
+              <path
+                d="M56.422 44.237c-8.244-4.424-10.1-5.335-13.13-10.762 6.237-.42 10.036-1.416 16.708-4.36-9.354.291-11.417.433-16.752-2.753 5.193-3.479 7.989-6.246 12.292-12.128-7.953 4.929-9.667 6.081-15.884 5.996 2.76-5.612 3.791-9.404 4.581-16.652-4.424 8.244-5.335 10.1-10.762 13.131-.42-6.238-1.416-10.037-4.36-16.709.291 9.354.433 11.417-2.753 16.752-3.479-5.193-6.246-7.989-12.128-12.292 4.929 7.953 6.081 9.667 5.996 15.884-5.612-2.76-9.404-3.792-16.652-4.581 8.244 4.424 10.108 5.335 13.131 10.755-6.238.42-10.037 1.416-16.709 4.36 9.354-.291 11.417-.433 16.752 2.753C11.559 37.11 8.763 39.877 4.46 45.76c7.953-4.93 9.667-6.081 15.884-5.989-2.76 5.612-3.792 9.404-4.581 16.652 4.424-8.244 5.335-10.1 10.762-13.13.42 6.237 1.416 10.036 4.36 16.708-.291-9.354-.433-11.417 2.753-16.752 3.479 5.193 6.246 7.989 12.128 12.292-4.929-7.953-6.081-9.667-5.989-15.884 5.612 2.76 9.404 3.791 16.652 4.581h-.007Z"
+                fill="#000"
+              />
+            </svg>
+          </Link>
+          <p className="hide-mobile">Creative E-Commerce</p>
         </div>
-        {settings.emailAddress ? (
-          <h1>{settings.emailAddress}</h1>
-        ) : (
-          <p>No email provided</p>
-        )}
-
+        <nav className="header__nav">
+          <Link href="/#work" className="header__link">
+            Work
+          </Link>
+          <Link href="about" className="header__link">
+            About
+          </Link>
+          <a
+            href={`mailto:${settings.emailAddress}`}
+            target="_blank"
+            className="btn btn--small blob-btn"
+          >
+            <span className="text show-mobile">Contact</span>
+            <span className="text hide-mobile">Get in Touch</span>
+            <div className="blob-btn__blobs">
+              <div className="blob-btn__blob"></div>
+              <div className="blob-btn__blob"></div>
+              <div className="blob-btn__blob"></div>
+            </div>
+          </a>
+        </nav>
+      </header>
+      <main>{children}</main>
+      <footer>
         <CTABanner bannerText={settings?.wavyBannerText}></CTABanner>
 
         <section className="contact">
           <div className="contact__inner">
             <h3 className="section__title--medium" data-reveal="lines-masked">
-              Let’s work together! Now booking projects starting in Q1 and Q2
-              2023.
+              <PortableText value={settings?.contactText} />
             </h3>
             <a
-              href="mailto:hello@sarahholden.studio"
+              href={`mailto:${settings.emailAddress}`}
               target="_blank"
               className="btn blob-btn blob-button--light"
             >
-              <span className="text">hello@sarahholden.studio</span>
+              {settings.emailAddress && (
+                <span className="text">{settings.emailAddress}</span>
+              )}
+
               <div className="blob-btn__blobs">
                 <div className="blob-btn__blob"></div>
                 <div className="blob-btn__blob"></div>
@@ -60,7 +85,7 @@ export default function Layout({
           </div>
         </section>
         <section className="copyright">
-          <p>✿ Site by me (Sarah). Thanks for visiting. ✿</p>
+          <PortableText value={settings?.footer} />
         </section>
       </footer>
     </div>
