429dc6fb74420b65851f5c6fff2a615e4db3cc80
Sarah Holden <sarahbethholden@gmail.com>
2023-10-18 16:15:57 -0700

    Updates for hero component

--- src/components/shared/Hero.tsx
+++ src/components/shared/Hero.tsx
@@ -1,3 +1,6 @@
+import Image from 'next/image'
+
+import { urlForImage } from '~/lib/sanity.image'
 import { HeroType } from '~/types'
 
 export default function Hero({ value }: { value: HeroType }) {
@@ -9,24 +12,16 @@ export default function Hero({ value }: { value: HeroType }) {
           {headline1}
         </h1>
         <div className="about-hero-image">
-          {/* <picture className="media">
-							<source
-							sizes="(max-width: 749px) 54vw, 25vw"
-							srcset="img/about_portrait.jpg?as=avif&width=220 220w, img/about_portrait.jpg?as=avif&width=350 350w, img/about_portrait.jpg?as=avif&width=420 420w, img/about_portrait.jpg?as=avif&width=600 600w, img/about_portrait.jpg?as=avif&width=800 800w"
-							type="image/avif" />
-							<source
-							sizes="(max-width: 749px) 54vw, 25vw"
-							srcset="img/about_portrait.jpg?as=webp&width=220 220w, img/about_portrait.jpg?as=webp&width=350 350w, img/about_portrait.jpg?as=webp&width=420 420w, img/about_portrait.jpg?as=webp&width=600 600w, img/about_portrait.jpg?as=webp&width=800 800w"
-							type="image/webp" />
-							<source
-							sizes="(max-width: 749px) 54vw, 25vw"
-							srcset="img/about_portrait.jpg?&width=220 220w, img/about_portrait.jpg?&width=350 350w, img/about_portrait.jpg?&width=420 420w, img/about_portrait.jpg?&width=600 600w, img/about_portrait.jpg?&width=800 800w"
-							type="image/jpg" />
-							<img
-							width="351" height="422" decoding="async"
-							className="hero-portrait"
-							src="img/about_portrait.jpg?width=350" alt="Sarah Headshot" />
-						</picture> */}
+          <Image
+            width="351"
+            height="422"
+            decoding="async"
+            className="hero-portrait"
+            sizes="(max-width: 749px) 54vw, 25vw"
+            src={urlForImage(image).url()}
+            alt="Sarah Headshot"
+          />
+
           <div className="image-swipe">&nbsp;</div>
         </div>
         <h1 id="hero-quote-2" className=" section__title--large section__title">
