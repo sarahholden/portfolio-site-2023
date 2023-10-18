c9127132436e730345caf461669400f5de0c5574
Sarah Holden <sarahbethholden@gmail.com>
2023-10-17 17:11:51 -0700

    Reorg  - moving Types

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,13 +1,13 @@
 import { SchemaTypeDefinition } from 'sanity'
 
-import blockContent from './blockContent'
 import page from './documents/page'
+import post from './documents/post'
+import project from './documents/project'
+import blockContent from './objects/blockContent'
 import ctaBanner from './objects/ctaBanner'
 import hero from './objects/hero'
 import infoList from './objects/infoList'
 import numberedList from './objects/numberedList'
-import post from './post'
-import project from './project'
 import home from './singletons/home'
 import settings from './singletons/settings'
 
