a15a569614c07234d28e0f6e6a37a39bf28ffeaf
Sarah Holden <sarahbethholden@gmail.com>
2023-10-18 11:21:51 -0700

    Porting over footer and banner text from current site, starting to build out components

---
+++ src/components/shared/CTABanner.tsx
@@ -0,0 +1,33 @@
+export default function CTABanner({ bannerText }) {
+  return (
+    <section className="wavy-divider">
+      <a href="about.html">
+        <svg
+          className="svgtext svgtext--wavy"
+          width="120%"
+          preserveAspectRatio="xMidYMid meet"
+          viewBox="0 0 1690 120"
+        >
+          <path
+            id="text-curve11"
+            d="M0 1.5c120.72 0 120.72 74 241.43 74s120.72-74 241.43-74 120.72 74 241.43 74 120.72-74 241.43-74 120.71 74 241.42 74 120.72-74 241.43-74 120.71 74 241.43 74"
+            fill="none"
+          ></path>
+          <text>
+            <textPath href="#text-curve11">
+              {bannerText} • {bannerText} • {bannerText} • {bannerText}
+            </textPath>
+          </text>
+        </svg>
+        <div className="wavy-divider__inner">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1690 201">
+            <path
+              d="M1690 201c-120.71 0-120.71-74-241.43-74s-120.71 74-241.43 74-120.71-74-241.42-74-120.71 74-241.43 74-120.72-74-241.43-74-120.72 74-241.43 74S120.72 127 0 127V0c120.72 0 120.72 74 241.43 74S362.15 0 482.86 0s120.72 74 241.43 74S845.01 0 965.72 0s120.71 74 241.42 74 120.72-74 241.43-74S1569.28 74 1690 74v127Z"
+              fill="#1E5D43"
+            />
+          </svg>
+        </div>
+      </a>
+    </section>
+  )
+}
--- src/components/shared/Layout.tsx
+++ src/components/shared/Layout.tsx
@@ -2,6 +2,8 @@ import { PortableText } from '@portabletext/react'
 
 import { SettingsPayload } from '~/types'
 
+import CTABanner from './CTABanner'
+
 export interface LayoutProps {
   children: React.ReactNode
   settings: SettingsPayload | undefined
@@ -34,6 +36,32 @@ export default function Layout({
         ) : (
           <p>No email provided</p>
         )}
+
+        <CTABanner bannerText={settings?.wavyBannerText}></CTABanner>
+
+        <section className="contact">
+          <div className="contact__inner">
+            <h3 className="section__title--medium" data-reveal="lines-masked">
+              Let’s work together! Now booking projects starting in Q1 and Q2
+              2023.
+            </h3>
+            <a
+              href="mailto:hello@sarahholden.studio"
+              target="_blank"
+              className="btn blob-btn blob-button--light"
+            >
+              <span className="text">hello@sarahholden.studio</span>
+              <div className="blob-btn__blobs">
+                <div className="blob-btn__blob"></div>
+                <div className="blob-btn__blob"></div>
+                <div className="blob-btn__blob"></div>
+              </div>
+            </a>
+          </div>
+        </section>
+        <section className="copyright">
+          <p>✿ Site by me (Sarah). Thanks for visiting. ✿</p>
+        </section>
       </footer>
     </div>
   )
